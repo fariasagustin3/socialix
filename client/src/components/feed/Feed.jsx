dedaf433f9cc8aeb6079a58b3a5793e58ab01b01
fariasagustin3 <fariasagustin3@gmail.com>
2024-03-12 11:09:51 -0300

    feat(components): like and dislike function

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Post from '../post/Post'
 import Share from '../share/Share'
 import axios from 'axios';
+import { AuthContext } from '../../context/AuthContext';
 import './feed.css'
 
 export default function Feed({ username }) {
   const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchPosts = async() => {
       const res = username
         ? await axios.get(`http://localhost:8800/api/posts/profile/${username}`)
-        : await axios.get("http://localhost:8800/api/posts/timeline/65ea0184b366833e6cf37da6")
+        : await axios.get(`http://localhost:8800/api/posts/timeline/${user._id}`)
       setPosts(res.data);
     }
 
     fetchPosts();
-  }, [username])
+  }, [username, user._id])
 
   return (
     <div className='feed'>
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,17 +1,28 @@
 import "./post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from 'axios';
 import { format } from 'timeago.js';
 import { Link } from 'react-router-dom';
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Post({ post }) {
   const [like, setLike] = useState(post?.likes?.length)
   const [isLiked, setIsLiked] = useState(false)
   const [user, setUser] = useState({})
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user: currentUser } = useContext(AuthContext);
 
-  const likeHandler = () => {
+  useEffect(() => {
+    setIsLiked(post.likes.includes(currentUser._id))
+  }, [currentUser._id, post.likes])
+
+  const likeHandler = async () => {
+    try {
+      axios.put(`http://localhost:8800/api/posts/${post._id}/like`, { userId: currentUser._id })
+    } catch(error) {
+      console.log(error);
+    }
     setLike(isLiked ? like - 1 : like + 1)
     setIsLiked(!isLiked)
   }
@@ -31,7 +42,7 @@ export default function Post({ post }) {
         <div className="postTop">
           <div className="postTopLeft">
             <Link to={`/profile/${user?.username}`} style={{ cursor: 'pointer' }}>
-              <img src={user?.profilePicture || PUBLIC_FOLDER + "person/noAvatar.png"} alt="" className="postProfileImg" />
+              <img src={user.profilePicture ? PUBLIC_FOLDER + user?.profilePicture : PUBLIC_FOLDER + "person/noAvatar.png"} alt="" className="postProfileImg" />
             </Link>
             <span className="postUsername">{user?.username} -</span>
             <span className="postDate">{format(post?.createdAt)}</span>
