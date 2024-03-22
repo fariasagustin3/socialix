da8035c6f27980f847d8fd956ec66f4eaa788f35
fariasagustin3 <fariasagustin3@gmail.com>
2024-03-22 10:24:47 -0300

    feat(messages): send a new message with web sockets

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -11,71 +11,98 @@ import { io } from 'socket.io-client'
 export default function Messenger() {
   const [conversations, setConversations] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
-  const [messages, setMessages] = useState(null);
-  const [newMessage, setNewMessage] = useState(null);
-  const socket = useRef(io("ws://localhost:8900"))
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const socket = useRef();
   const { user } = useContext(AuthContext);
+  const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:8900")
-  }, [])
+    socket.current = io("ws://localhost:8900");
+    socket.current.on("getMessage", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now(),
+      });
+    });
+  }, []);
 
   useEffect(() => {
-    socket.current.emit("addUser", user._id)
-    socket.current.on("getUsers", users => {
-      console.log(users)
-    })
-  }, [user])
+    arrivalMessage &&
+      currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage, currentChat]);
 
-  const scrollRef = useRef()
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const message = {
-      sender: user._id,
-      text: newMessage,
-      conversationId: currentChat._id
-    }
-    try {
-      const res = await axios.post(`${process.env.REACT_APP_API}messages`, message);
-      setMessages([...messages, message])
-      setNewMessage("")
-    } catch(err) {
-      console.log(err)
-    }
-  }
+  useEffect(() => {
+    socket.current.emit("addUser", user._id);
+    socket.current.on("getUsers", (users) => {
+      setOnlineUsers(
+        user.followings.filter((f) => users.some((u) => u.userId === f))
+      );
+    });
+  }, [user]);
 
   useEffect(() => {
     const getConversations = async () => {
       try {
-        const res = await axios.get(
-          `${process.env.REACT_APP_API}conversations/${user._id}`
-        );
+        const res = await axios.get(`${process.env.REACT_APP_API}conversations/` + user._id);
         setConversations(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-
     getConversations();
   }, [user._id]);
 
   useEffect(() => {
     const getMessages = async () => {
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API}messages/${currentChat?._id}`)
-        setMessages(res.data)
+        const res = await axios.get(`${process.env.REACT_APP_API}messages/` + currentChat?._id);
+        setMessages(res.data);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    };
+    getMessages();
+  }, [currentChat]);
 
-    getMessages()
-  }, [currentChat])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const message = {
+      sender: user._id,
+      text: newMessage,
+      conversationId: currentChat._id,
+    };
+
+    const receiverId = currentChat.members.find(
+      (member) => member !== user._id
+    );
+
+    socket.current.emit("sendMessage", {
+      senderId: user._id,
+      receiverId,
+      text: newMessage,
+    });
+
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API}messages`, message);
+      setMessages([...messages, res.data]);
+      setNewMessage("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" })
-  }, [messages])
+    if(scrollRef.current) {
+      return scrollRef.current.scrollIntoView({ behavior: "smooth" })
+    } else {
+      return
+    }
+  }, [messages]);
 
   return (
     <>
